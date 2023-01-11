fc10c61e1c8dcb9e8c73fa53c608eb01c3274187
Oleg Kolpakov <kolpakovmail@gmail.com>
2023-01-11 18:05:45 +0530

    1470. Shuffle the Array

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-// 1108. Defanging an IP Address
-
-// Given a valid (IPv4) IP address, return a defanged version of that IP address.
-// A defanged IP address replaces every period "." with "[.]".
-
-// Example 1
-// Input: address = "1.1.1.1"
-// Output: "1[.]1[.]1[.]1"
-
-// Example 2
-// Input: address = "255.100.50.0"
-// Output: "255[.]100[.]50[.]0"
-
-// Constraints:
-// The given address is a valid IPv4 address.
+// 1470. Shuffle the Array
 
 /**
- * @param {string} address
- * @return {string}
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
  */
-var defangIPaddr = function(address) {
-    return address.split('.').join('[.]');
-    // OR
-    // return address.replaceAll('.', '[.]');
+var shuffle = function(nums, n) {
+
+    let ans = [];
+    
+    for (let i = 0; i < n; i++) {
+        ans.push(nums[i]);
+        ans.push(nums[i+n]);
+    }
+    return ans;
 };
 
-console.log(defangIPaddr("1.1.1.1")); // 1[.]1[.]1[.]1
-console.log(defangIPaddr( "255.100.50.0")); // 255[.]100[.]50[.]0
+console.log(shuffle([2,5,1,3,4,7], 3));    // [2,3,5,4,1,7]
+console.log(shuffle([1,2,3,4,4,3,2,1], 4));    // [1,4,2,3,3,2,4,1]
+console.log(shuffle([1,1,2,2], 2));  // [1,2,1,2]
