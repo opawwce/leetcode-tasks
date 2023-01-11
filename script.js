4ce36747ee562644b66b145a1c104c7d17fcea09
Oleg Kolpakov <kolpakovmail@gmail.com>
2023-01-11 13:26:40 +0530

    1929. Concatenation of Array

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-// 1920. Build Array from Permutation
+// 1929. Concatenation of Array
 
-// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+// Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 
-// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+// Specifically, ans is the concatenation of two nums arrays.
+
+// Return the array ans.
 
 // Example 1
-// Input: nums = [0,2,1,5,3,4]
-// Output: [0,1,2,4,5,3]
-// Explanation: The array ans is built as follows: 
-// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
-//     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
-//     = [0,1,2,4,5,3]
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
 
 // Example 2
-// Input: nums = [5,0,1,2,3,4]
-// Output: [4,5,0,1,2,3]
-// Explanation: The array ans is built as follows:
-// ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
-//     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
-//     = [4,5,0,1,2,3]
+// Input: nums = [1,3,2,1]
+// Output: [1,3,2,1,1,3,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+// - ans = [1,3,2,1,1,3,2,1]
+
 
 // Constraints:
-// 1 <= nums.length <= 1000
-// 0 <= nums[i] < nums.length
-// The elements in nums are distinct.
+// n == nums.length
+// 1 <= n <= 1000
+// 1 <= nums[i] <= 1000
 
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-var buildArray = function(nums) {
-    var ans = [];
-    for (let i = 0; i < nums.length; i++) {
-        ans.push(nums[nums[i]]);
+var getConcatenation = function(nums) {
+    let ans = [];
+
+    for (let i = 0; i < nums.length*2; i++) {
+        if (i < nums.length) {
+            ans[i] = nums[i];
+        } else {
+            ans[i] = nums[i - nums.length];
+        }
     }
     return ans;
+
+    // Solution 2
+    // return nums.concat(nums);
+    // or return [...nums, ...nums];
 };
 
-console.log(buildArray([0,2,1,5,3,4])); // [0,1,2,4,5,3]
-console.log(buildArray([5,0,1,2,3,4])); // [4,5,0,1,2,3]
+console.log(getConcatenation([1,2,1])); // [1,2,1,1,2,1]
+console.log(getConcatenation([1,3,2,1])); // [1,3,2,1,1,3,2,1]
